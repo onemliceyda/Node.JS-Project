0e3f591a2bb83bfd49ae40ff1cbf9cadb3c3f2e7
Ceyda <ceydaonemli2@gmail.com>
2022-01-17 22:09:51 +0300

    Created Node.js web server.

---
+++ nodeblog/app.js
@@ -0,0 +1,17 @@
+const http=require('http')
+
+const hostname='127.0.0.1'
+const port=3000
+
+const server=http.createServer( (req,res) =>{
+
+console.log(req.url)
+res.statusCode=200;
+res.setHeader('Content-Type','text/plain')
+res.end('Hello NODE.JS')
+
+})
+
+server.listen(port,hostname,()=> {
+    console.log(`Server Çalışıyor, http://${hostname}:${port}/`)
+})
