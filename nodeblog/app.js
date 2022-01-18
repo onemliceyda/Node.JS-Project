507ae128a37b628900969e524c4a1e1a89657a0e
Ceyda <ceydaonemli2@gmail.com>
2022-01-18 16:07:39 +0300

    Sample html pages created.

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -1,17 +1,32 @@
-const http=require('http')
+const http = require('http')
+const fs = require('fs')
 
-const hostname='127.0.0.1'
-const port=3000
 
-const server=http.createServer( (req,res) =>{
+const hostname = '127.0.0.1'
+const port = 3000
 
-console.log(req.url)
-res.statusCode=200;
-res.setHeader('Content-Type','text/plain')
-res.end('Hello NODE.JS')
+const indexPage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const notfoundPage = fs.readFileSync('404.html')
+const server = http.createServer((req, res) => {
 
+    if (req.url === '/') {
+        return res.end(indexPage)
+    }
+    else if (req.url === '/about') {
+        return res.end(aboutPage)
+
+    }
+    else if (req.url === '/contact') {
+        return res.end(contactPage)
+    }
+    else {
+        res.statusCode = 404;
+        res.end(notfoundPage)
+    }
 })
 
-server.listen(port,hostname,()=> {
+server.listen(port, hostname, () => {
     console.log(`Server Çalışıyor, http://${hostname}:${port}/`)
 })
