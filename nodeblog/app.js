21dacbc2e3bb32c4897e067353178ebb00f049ab
Ceyda <ceydaonemli2@gmail.com>
2022-01-19 14:26:47 +0300

    npm,express.js added and started the project.

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -1,32 +1,28 @@
-const http = require('http')
-const fs = require('fs')
-
-
-const hostname = '127.0.0.1'
+const path = require('path')
+const express = require('express')
+const app = express()
 const port = 3000
+const hostname = '127.0.0.1'
 
-const indexPage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notfoundPage = fs.readFileSync('404.html')
-const server = http.createServer((req, res) => {
+app.use(express.static('public'))
 
-    if (req.url === '/') {
-        return res.end(indexPage)
-    }
-    else if (req.url === '/about') {
-        return res.end(aboutPage)
-
-    }
-    else if (req.url === '/contact') {
-        return res.end(contactPage)
-    }
-    else {
-        res.statusCode = 404;
-        res.end(notfoundPage)
-    }
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'site/index.html'))
+})
+app.get('/blog', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'site/about.html'))
 })
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server Çalışıyor, http://${hostname}:${port}/`)
 })
+
+
+
+
+
+
+
