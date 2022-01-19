e7d35689935e9c03d630362d071653132fa18a30
Ceyda <ceydaonemli2@gmail.com>
2022-01-19 15:47:25 +0300

    Template Engine-Handlebars Express

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -1,19 +1,24 @@
 const path = require('path')
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 const hostname = '127.0.0.1'
 
 app.use(express.static('public'))
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'site/index.html'))
-})
-app.get('/blog', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+    res.render('site2/index')
 })
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'site/about.html'))
+    res.render('site2/about')
+})
+app.get('/blog', (req, res) => {
+    res.render('site2/blog')
 })
 
 app.listen(port, hostname, () => {
