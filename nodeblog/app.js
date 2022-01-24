9b87b5254cfdaec636689ae499e291788ce724d5
Ceyda <ceydaonemli2@gmail.com>
2022-01-24 17:48:30 +0300

    login and register handlebars created.

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -12,13 +12,25 @@ app.set('view engine', 'handlebars');
 
 
 app.get('/', (req, res) => {
-    res.render('site2/index')
+    res.render('site/index')
 })
 app.get('/about', (req, res) => {
-    res.render('site2/about')
+    res.render('site/about')
 })
 app.get('/blog', (req, res) => {
-    res.render('site2/blog')
+    res.render('site/blog')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('site/contact')
+})
+
+app.get('/login', (req, res) => {
+    res.render('site/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('site/register')
 })
 
 app.listen(port, hostname, () => {
