6eb8bffffc8349fe8e176ecfbecb7f91fcc588cc
Ceyda <ceydaonemli2@gmail.com>
2022-02-14 15:04:33 +0300

    crud operations

--- nodeblog/test.js
+++ nodeblog/test.js
@@ -8,11 +8,29 @@ mongoose.connect('mongodb://127.0.0.1/nodeblog_test_db',
     useUnifiedTopology:true
 })
 
-Post.create(
+Post.findByIdAndDelete('620a3d591794d61af2f9db87',(error,post)=>{
+    console.log(error,post)
+})
+
+/*Post.findById('620a3d591794d61af2f9db87',(error,post)=>{
+    console.log(error,post)
+}) */
+
+/*Post.findByIdAndUpdate('620a3d591794d61af2f9db87',{
+title:"Benim ilk postum"
+},(error,post)=> {
+    console.log(error,post)
+})
+
+Post.find({},(error,post)=> {
+    console.log(error,post);
+}) */
+
+/*Post.create(
     {
-        title:"My First Post Title",
-        content:"Post content,lorem ipsum text",
+        title:"Second Post Title",
+        content:"Second Post content,lorem ipsum text",
         
     },(error,post)=>{
         console.log(error,post)
-    })
+    }) */
